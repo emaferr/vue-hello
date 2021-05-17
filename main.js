1c491652675a64bf0be33d07c4befe8577a81bce
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-05-17 15:57:38 +0200

    Stampo a schermo un messaggio all’interno di un h1

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 // Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
+let app = new Vue(
+
+
+    {
+        el : '#root',
+
+        data : {
+
+            nomeUtente : "emanuele", //prompt("Inserisci il tuo nome"), 
+            benvenuto : "benvenuto sullo Shop!",
+
+        },
+
+    },
+
+    
+   
+)
 
 // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
